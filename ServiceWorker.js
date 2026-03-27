d8cce79bab3703986a25154d5de0cd0e9d8b29f7
sebqu5 <sebastian_qu@yahoo.com>
2026-03-27 18:13:01 +0000

    Service worker bug fix

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -9,12 +9,14 @@ const urlsToCache = [
   './icon-512.png'
 ];
 
-// Install service worker and cache files
-self.addEventListener("install", event => {
-  self.skipWaiting();
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-// Fetch files from cache first, fallback to network
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
