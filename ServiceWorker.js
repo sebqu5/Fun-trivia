cf4f185d4f66773ca8d51837a7e641519f7b6195
sebqu5 <sebastian_qu@yahoo.com>
2026-03-27 18:28:19 +0000

    Service worker bugfix

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,22 +1,28 @@
-const CACHE_NAME = 'FunTrivia-cache-v1';
+const CACHE_NAME = 'funtrivia-cache-v1';
 const urlsToCache = [
-  './',                          // index.html
-  './Build/FunTriviaBuild.loader',       // Unity loader
-  './Build/FunTriviaBuild.data',        // Unity data file
-  './Build/FunTriviaBuild.wasm',        // Unity wasm file
-  './Build/FunTriviaBuild.framework.js',// Unity framework
-  './icon-192.png',
-  './icon-512.png'
+  '/', // root page
+  '/index.html',
+  '/Build/FunTriviaBuild.wasm',
+  '/Build/FunTriviaBuild.data',
+  '/Build/FunTriviaBuild.framework.js',
+  // add StreamingAssets or icons if you use them
+  '/StreamingAssets/', 
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
+// Install event – cache all necessary files
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// Fetch event – serve from cache if available
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
